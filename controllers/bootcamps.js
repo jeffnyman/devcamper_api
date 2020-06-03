9856c78e28296fd54b7a36c7ec23cbc83b9a462f
Jeff Nyman <jeffnyman@gmail.com>
2020-06-03 10:32:11 -0500

    Providing try/catch around update logic for bootcamps.

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -59,22 +59,26 @@ exports.createBootcamp = async (req, res, next) => {
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
 exports.updateBootcamp = async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
-  });
+  try {
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
 
-  if (!bootcamp) {
-    return res.status(400).json({ success: false });
+    if (!bootcamp) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (err) {
+    res.status(400).json({ success: false, msg: "Unable to update Bootcamp." });
   }
-
-  res.status(200).json({ success: true, data: bootcamp });
 };
 
 // @desc    Delete existing bootcamp.
 // @route   DELETE /api/v1/bootcamps/:id
 // @access  Private
-exports.deleteBootcamp = (req, res, next) => {
+exports.deleteBootcamp = async (req, res, next) => {
   res
     .status(200)
     .json({ success: true, msg: `Delete Bootcamp ${req.params.id}` });
